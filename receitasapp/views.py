01753e2825f805d1b06edcf1e2edb53f6ee5be40
Silas Lago <juanfsa@live.com>
2022-10-05 19:01:39 -0300

    partials and not mocked data recipes

--- receitasapp/views.py
+++ receitasapp/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, "index.html")
+
+    data_receitas = {
+        1: 'Sorvete',
+        2: 'Lasanha',
+        3: 'Alcachofra'
+    }
+
+    data = {
+        'receitas': data_receitas
+    }
+
+    return render(request, "index.html", data)
 
 def receita(request):
     return render(request, 'receita.html')
